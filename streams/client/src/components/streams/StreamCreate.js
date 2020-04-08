02fc2a6d29b6a0955c136fa63bfab8ddb3ec620a
congcocz <congcong.c.zhang@nokia-sbell.com>
2020-04-08 11:22:54 +0800

    streams app rest-based pending part

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,74 +1,20 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form"; //这里Field大写是因为它是一个component。redux小写是因为它是一个函数，类似connect的函数，绑定action和mapStateToProps
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError = ({ touched, error }) => {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  };
-
-  //meta上存储了输入元素的一些属性纸，是否被touch啊，是否有error啊之类的
-  renderInput = ({ input, label, meta }) => {
-    //在这里添加一些css属性，让表格有error产生时，对应的输入框也会有红色提示
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
   onSubmit = formValues => {
     this.props.createStream(formValues);
   };
   render() {
     return (
-      //这里form的的className上必须添加error这个样式，否则semantic UI会自动阻止显示表格上所有的error
-      <form
-        className="ui form error"
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-      >
-        <Field
-          name="title"
-          component={this.renderInput}
-          label="Enter title"
-        ></Field>
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter description"
-        ></Field>
-        <button className="ui button primary" type="submit">
-          Submit
-        </button>
-      </form>
+      <div>
+        <h3>Create a stream</h3>
+        <StreamForm onSubmit={this.onSubmit}></StreamForm>
+      </div>
     );
   }
 }
 
-const validate = formValues => {
-  const errors = {};
-  if (!formValues.title) {
-    errors.title = "You must enter a title";
-  }
-  if (!formValues.description) {
-    errors.description = "You must enter a description";
-  }
-  return errors;
-};
-
-//这里reduxForm和connect一样，已经对该component进行了一次封装，所以这里单独把reduxForm拎出来，然后再用connect对其封装
-const formWrapper = reduxForm({
-  form: "StreamCreate",
-  validate
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapper);
+export default connect(null, { createStream })(StreamCreate);
--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,13 +1,38 @@
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
-const StreamEdit = props => {
-  console.log(props);
-  return <div>StreamEdit</div>;
-};
+class StreamEdit extends React.Component {
+
+  onSubmit = formValues => {
+    this.props.editStream(this.props.stream.id, formValues);
+  }
+
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading</div>;
+    }
+    return (
+      <div>
+        <h3>Edit a stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, "title", "description")}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+
+}
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
